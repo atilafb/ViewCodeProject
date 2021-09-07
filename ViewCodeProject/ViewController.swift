2620cce2f0a5879995a85b90b385b97f81c41912
atilafb <atilafb77@gmail.com>
2021-09-07 03:26:57 -0300

    Initial Commit

---
+++ ViewCodeProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ViewCodeProject
+//
+//  Created by Stelio Bastos on 07/09/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
